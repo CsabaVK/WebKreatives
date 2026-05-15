5f2c154358e2f945a7b16b287c9520fc28c85b78
Csaba G <karcvagany@gmail.com>
2026-05-15 21:20:05 +0200

    new og img, fixed v3.2

--- og/generate-og-png.js
+++ og/generate-og-png.js
@@ -2,19 +2,67 @@ const { chromium } = require('../node_modules/playwright-core');
 const path = require('path');
 const fs = require('fs');
 
-const svgPath = path.resolve(__dirname, 'og-image.svg');
-const svgContent = fs.readFileSync(svgPath, 'utf8');
+const logoB64 = fs.readFileSync(path.resolve(__dirname, '../assets/Horizontallogo.png')).toString('base64');
+const logoSrc = `data:image/png;base64,${logoB64}`;
 
 const html = `<!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <style>
-* { margin: 0; padding: 0; }
-body { width: 1200px; height: 630px; overflow: hidden; background: #0d0d0d; }
+* { margin: 0; padding: 0; box-sizing: border-box; }
+
+body {
+  width: 1200px;
+  height: 630px;
+  overflow: hidden;
+  background: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  font-family: -apple-system, 'Helvetica Neue', Arial, sans-serif;
+}
+
+.bar {
+  position: absolute;
+  top: 0; left: 0; right: 0;
+  height: 5px;
+  display: flex;
+}
+.bar-r { flex: 1; background: #df3821; }
+.bar-l { flex: 1; background: #b9e185; }
+
+.bar-btm {
+  position: absolute;
+  bottom: 0; left: 0; right: 0;
+  height: 5px;
+  display: flex;
+}
+.bar-btm .bar-r { background: #b9e185; }
+.bar-btm .bar-l { background: #df3821; }
+
+.logo {
+  width: 520px;
+  display: block;
+}
+
+.domain {
+  margin-top: 36px;
+  font-size: 18px;
+  font-weight: 500;
+  color: #999;
+  letter-spacing: 0.08em;
+}
 </style>
 </head>
-<body>${svgContent}</body>
+<body>
+  <div class="bar"><div class="bar-r"></div><div class="bar-l"></div></div>
+  <img class="logo" src="${logoSrc}">
+  <div class="domain">webkreatives.com</div>
+  <div class="bar-btm"><div class="bar-r"></div><div class="bar-l"></div></div>
+</body>
 </html>`;
 
 (async () => {
